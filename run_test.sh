70c11790022264042cd14296a4524e8ef0866576
Naman Bansal <bnaman50@gmail.com>
2021-04-22 00:59:44 -0500

    minor

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@ mkdir logs/
 export CUDA_VISIBLE_DEVICES=0
 
 nohup python -u test_model.py \
-    --model_path=./lightning_logs/version_2/checkpoints/best_model_checkpoint-epoch=04-val_loss=294225.34375-step_count=0.ckpt \
+    --model_path=./models/checkpoints/best_model_checkpoint-epoch=04-val_loss=294225.34375-step_count=0.ckpt \
     --num_workers=10 \
     --pin_memory \
     --test_batch_size=500 \
